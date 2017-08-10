38983cd0bd8f530b2a3fd254c37ac0c0507279e9
LWH <960023815@qq.com>
2017-08-10 09:57:10 -0400

    change .h

--- mem_check.h
+++ mem_check.h
@@ -1,17 +1,18 @@
+#ifndef __MEM_ALLOC_CHECK_H__
+#define __MEM_ALLOC_CHECK_H__
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-#ifndef __MEM_ALLOC_CHECK_H__
-#define __MEM_ALLOC_CHECK_H__
+
 
 #define malloc(_size) mmalloc(_size , __FILE__, __LINE__)
 #define free(_p) mfree(_p)
 
-extern mdump(void);
-
-#endif
 
 #if defined(__cplusplus)
 }
+#endif
+
 #endif
